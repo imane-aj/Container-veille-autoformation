266867d9a5434f36fc250be3455c3550d877aba7
imane-aj <ajroudi.im@gmail.com>
2022-10-26 08:15:43 -0700

    reactJs

---
+++ autoformationReactJs/AllinOne/src/components/functionComponents/Events.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Events() {
+    const [name, setName] = useState({name: 'imane'}); 
+    function click(){
+        
+    }
+  return (
+    <div>Events</div>
+  )
+}
+
+export default Events
